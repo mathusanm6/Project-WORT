02212eb41be939effb1d15187cb7b06e3030655d
mathusanm6 <hi@mathusan.me>
2025-03-29 13:44:10 +0100

    fix

--- src/rasptank/hardware/tracking_module.py
+++ src/rasptank/hardware/tracking_module.py
@@ -2,6 +2,8 @@ from enum import Enum
 
 from RPi import GPIO
 
+from src.common.logging.logger_api import Logger
+
 
 class TrackingModulePins(Enum):
     """Pin numbers for the tracking module."""
@@ -12,10 +14,16 @@ class TrackingModulePins(Enum):
 
 
 class TrackingModule:
-    def __init__(self):
+    def __init__(self, tracking_module_logger: Logger):
+        """Initialize the tracking module."""
+        self.logger = tracking_module_logger
+        self.logger.infow("Initializing tracking module")
+
         GPIO.setup(TrackingModulePins.RIGHT.value, GPIO.IN)
         GPIO.setup(TrackingModulePins.MIDDLE.value, GPIO.IN)
         GPIO.setup(TrackingModulePins.LEFT.value, GPIO.IN)
+
+        self.logger.infow("Tracking module initialized")
 
     def _get_tracking_module_status(self):
         status_right = GPIO.input(TrackingModulePins.RIGHT.value)
