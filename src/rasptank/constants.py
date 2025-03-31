9b5caa68931264d0c9d60996a98db7a0f4e29da6
SushiAtomik <jewin.cheng@gmail.com>
2025-03-31 11:10:19 +0200

    [fix] converting id to hex

--- src/rasptank/constants.py
+++ src/rasptank/constants.py
@@ -1,3 +1,10 @@
 import uuid
 
-TANK_ID = str(uuid.getnode())
+tank_id = hex(uuid.getnode())
+TANK_ID = (
+    str(tank_id)[:15]
+    if len(str(tank_id)) > 15
+    else (
+        str(tank_id) if len(str(tank_id)) == 15 else str(tank_id) + ("0" * (15 - len(str(tank_id))))
+    )
+)
